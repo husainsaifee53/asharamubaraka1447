c09848d31546821937b2a59a3af1695055400a14
Hussain Huzefa <155477247+husainsaifee53@users.noreply.github.com>
2025-06-19 21:46:43 +0530

    map embed link added

--- src/App.tsx
+++ src/App.tsx
@@ -210,8 +210,15 @@ function App() {
                 </div>
               </div>
               <div className="w-full h-96">
-   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.1966710287957!2d75.77434559999999!3d11.2469381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6593d62c6c7e9%3A0xe06557ab3fe51cc6!2sMohammedi%20Masjid%2C%20Calicut%20(Dawoodi%20Bohra)!5e0!3m2!1sen!2sin!4v1750349406270!5m2!1sen!2sin" 
-     width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.1966710287957!2d75.77434559999999!3d11.2469381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6593d62c6c7e9%3A0xe06557ab3fe51cc6!2sMohammedi%20Masjid%2C%20Calicut%20(Dawoodi%20Bohra)!5e0!3m2!1sen!2sin!4v1750349406270!5m2!1sen!2sin"
+      className="w-full h-full border-0"
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+{/*    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.1966710287957!2d75.77434559999999!3d11.2469381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6593d62c6c7e9%3A0xe06557ab3fe51cc6!2sMohammedi%20Masjid%2C%20Calicut%20(Dawoodi%20Bohra)!5e0!3m2!1sen!2sin!4v1750349406270!5m2!1sen!2sin" 
+     width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
   </div>
               <div className="p-6">
                 <h4 className="font-bold text-emerald-800 mb-4">How to Reach</h4>
